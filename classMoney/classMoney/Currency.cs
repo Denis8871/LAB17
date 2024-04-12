8a1b8535e513a852984807208aa56ec8a660e2b6
Denis8871 <145900110+Denis8871@users.noreply.github.com>
2024-04-12 09:15:50 +0300

    Add files via upload

---
+++ classMoney/classMoney/Currency.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace classMoney
+{
+    class Currency
+    {
+        double money;
+        int from, to;
+
+        public Currency(double _money, int _from, int _to)
+        {
+            this.money = _money;
+            this.from = _from;
+            this.to = _to;
+        }
+
+       public double getMoney()
+        {
+            switch (from)
+            {
+                case 0: switch (to) 
+                {
+                    case 1: money *= 0.025; break; //гривні-долари
+                    case 2: money *= 0.024; break; //гривні-евро
+                } break;
+
+                case 1: switch (to)
+                    {
+                        case 0: money *= 39.22; break; //долари-гривні
+                        case 2: money *= 0.93; break; //долари-евро
+                    } break;
+
+                case 2: switch (to)
+                    {
+                        case 0: money *= 42.02; break; //евро-гривні
+                        case 1: money *= 1.07; break; //евро-долари
+                    } break;
+            }
+            return money;
+        }
+    }
+}
---
+++ classMoney/classMoney/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace classMoney
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        void callConverter()
+        {
+            Currency curr = new Currency(Convert.ToDouble(textBox2.Text), comboBox1.SelectedIndex, comboBox2.SelectedIndex);
+            textBox3.Text = curr.getMoney().ToString();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            callConverter();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.SelectedIndex = comboBox1.Items.IndexOf(0);
+            comboBox1.Text = comboBox1.Items[0].ToString();
+
+            comboBox2.SelectedIndex = comboBox2.Items.IndexOf(1);
+            comboBox2.Text = comboBox2.Items[1].ToString();
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!Char.IsDigit(e.KeyChar) &&
+                !(Char.IsControl(e.KeyChar)))
+            {
+                if (textBox2.Text.Length == 0)
+                    e.Handled = true;
+                if (!((e.KeyChar.ToString() == ",") &&
+                    (textBox2.Text.IndexOf(",") == -1)) )
+                    e.Handled = true;
+                 
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0: pictureBox1.Image = Properties.Resources.RUB; break;
+                case 1: pictureBox1.Image = Properties.Resources.USD; break;
+                case 2: pictureBox1.Image = Properties.Resources.EUR; break;
+            }
+            if (textBox3.Text.Length != 0 && comboBox2.SelectedIndex != -1)
+                callConverter();
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (comboBox2.SelectedIndex)
+            {
+                case 0: pictureBox2.Image = Properties.Resources.RUB; break;
+                case 1: pictureBox2.Image = Properties.Resources.USD; break;
+                case 2: pictureBox2.Image = Properties.Resources.EUR; break;
+            }
+            if (textBox3.Text.Length != 0 && comboBox1.SelectedIndex != -1)
+                callConverter();
+        }
+    }
+}
